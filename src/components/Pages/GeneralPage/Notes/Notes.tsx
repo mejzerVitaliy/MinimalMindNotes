65fbfbe878e17b541534b2a1cbb8cdc493ba7d3c
mejzerVitaliy <mejzervitalik@gmail.com>
2024-08-02 22:44:53 +0300

    ттт

--- src/components/Pages/GeneralPage/Notes/Notes.tsx
+++ src/components/Pages/GeneralPage/Notes/Notes.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import FormButton from '../../../UI/Button/FormButton';
 import cl from './Notes.module.scss';
+import {CSSTransition, TransitionGroup} from 'react-transition-group'
 
 interface NotesPropsArray{
     notesArray: any[],
@@ -8,21 +9,35 @@ interface NotesPropsArray{
 }
 
 const Notes: React.FC<NotesPropsArray> = ({notesArray, deleteNote}) => {
+    const nodeRef = useRef(null);
 
     return (
-        <ul className={cl.notes}>
+        <TransitionGroup>
+            <ul className={cl.notes}>
             {
             notesArray.length
                 ? notesArray.map(note => (
-                    <li key={note.id} className={cl.note}>
-                        <h1>{note.title}</h1>
-                        <FormButton onClick={() => deleteNote(note.id)}>delete note</FormButton>
-                    </li>
+                    <CSSTransition
+                        in={true}
+                        key={note.id}
+                        timeout={500}
+                        classNames={cl.notesAnimation}
+                        nodeRef={nodeRef}
+                        unmountOnExit
+                    >
+                        <li key={note.id} ref={nodeRef} className={cl.note}>
+                            <h1>{note.title}</h1>
+                            <FormButton onClick={() => deleteNote(note.id)} className={cl.removeBtn}>delete note</FormButton>
+                        </li>
+                    </CSSTransition>
+                    
                 )).reverse()
                 : <h1 className={cl.haveNotNotes}> you don't have any notes yet </h1>
             }
-        </ul>
+            </ul>
+        </TransitionGroup>
+        
     )
 }
-
+// className={cl.note}
 export default Notes
