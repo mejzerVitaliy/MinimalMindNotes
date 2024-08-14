fdc462c9c63208dbd26fc5cbd1d9f364ffdeecb6
mejzerVitaliy <mejzervitalik@gmail.com>
2024-08-14 11:52:53 +0300

    *

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
-import jsonServer from 'json-server'
-const server = jsonServer.create();
-const router = jsonServer.router('db.json');
-const middlewares = jsonServer.defaults();
+import jsonServer from 'json-server';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const server = jsonServer.create();
+const router = jsonServer.router(path.join(__dirname, 'db.json'));
+
+const middlewares = jsonServer.defaults();
 server.use(middlewares);
 server.use(router);
 
