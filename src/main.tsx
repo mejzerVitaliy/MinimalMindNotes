570fc278dc2a61eba7622fd01628264ff0ed56ba
mejzerVitaliy <mejzervitalik@gmail.com>
2024-08-14 13:13:27 +0300

    finaly4

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,6 @@ import { AuthProvider } from './components/AuthProvider.tsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-    
     <React.StrictMode>
         <AuthProvider>
             <App />
