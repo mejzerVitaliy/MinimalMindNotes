e978799a88efcb99331fabfb690aa48f83d07af1
mejzerVitaliy <mejzervitalik@gmail.com>
2024-08-15 17:35:26 +0300

    fixed?

--- server.js
+++ server.js
@@ -6,23 +6,18 @@ import express from 'express';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// Создаем экземпляр express
 const app = express();
 
-// Настраиваем json-server
 const router = jsonServer.router(path.join(__dirname, 'db.json'));
 const middlewares = jsonServer.defaults();
-app.use('/api', middlewares, router); // Префикс /api для API маршрутов
+app.use('/api', middlewares, router); 
 
-// Обслуживаем статические файлы
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Для всех остальных маршрутов, возвращаем index.html
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// Запуск сервера
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
--- src/API/Api.ts
+++ src/API/Api.ts
@@ -1,13 +1,13 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL:'https://minimalmindnotes-1.onrender.com'
+    baseURL:'https://minimalmindnotes-1.onrender.com/api'
 })
 
 export const createUser = (userData: {}) => api.post('/users', userData)
 
 export const checkAuth = async (login: string, password: string) => {
-    const response = await api.get('/api/users', { params: { login } });
+    const response = await api.get('/users', { params: { login } });
     const users = response.data;
     const targetUser = users.find((user: { login: string, password: string }) => user.password === password);
     if (targetUser) return [targetUser]
@@ -16,7 +16,7 @@ export const checkAuth = async (login: string, password: string) => {
 
 export const checkLogins = async (login:string) => {
     try {
-        const response = await api.get('/api/users', { params: { login } })
+        const response = await api.get('/users', { params: { login } })
         const targetUserLogins = response.data
         if (!targetUserLogins) {
             return []
