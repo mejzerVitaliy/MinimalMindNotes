b09d03a29f78a8c3863dd59e20fffaa514981290
mejzerVitaliy <mejzervitalik@gmail.com>
2024-08-16 14:13:49 +0300

    //////

--- server.js
+++ server.js
@@ -10,17 +10,15 @@ const app = express();
 
 const router = jsonServer.router(path.join(__dirname, 'db.json'));
 const middlewares = jsonServer.defaults();
-
 app.use('/api', middlewares, router); 
 
 app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+    res.sendFile(path.join(__dirname, 'dist' ,'index.html'));
 });
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
-
